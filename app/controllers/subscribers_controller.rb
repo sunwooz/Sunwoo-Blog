624162471aef5ff0b82108170ff8300825470d05
sunwooz <yangsunwoo@gmail.com>
2016-05-11 11:19:33 -0400

    subscriber css changes

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,6 +3,7 @@ class SubscribersController < ApplicationController
   def create
     subscriber = Subscriber.new(subscriber_params)
     if subscriber.save
+      flash[:notice] = "Thanks for Subscribing!"
       SubscriberMailer.subscribed_notification(subscriber.email).deliver!
       redirect_to :back, notice: 'Thanks for subscribing!'
     else
